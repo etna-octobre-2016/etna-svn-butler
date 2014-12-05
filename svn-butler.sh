f742197494a9525ebdb2a0720f94c127b0d202b2
Said AHEMT <said.ahemt@nolaroads.com>
2014-12-05 17:25:38 +0100

    fixes timer seconds counting issue

--- svn-butler.sh
+++ svn-butler.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Ticker configuration
-TICKER_SVN_UP_MINUTES=2
+TICKER_SVN_COMMITS_MINUTES=2
+TICKER_SVN_UP_MINUTES=32
 
 # SVN repo informations
 SVN_TMP_DIR="svn_butler_tmp"
@@ -12,10 +13,8 @@ GIT_REPOSITORY_DIR="restful"
 GIT_REPOSITORY_URL="https://github.com/itadakimas/etna-2-restful.git"
 
 # And...action !
-TICKER_MINUTES=0
-TICKER_SECONDS=0
-TICKER_START_TIMESTAMP=$(date +%s)
 echo -e "\n#### SVN BUTLER [ON] ####\n"
+# svn up
 if [ -d $SVN_TMP_DIR ]; then
     echo -e "[DEBUG]\tRemoves old temporary directory"
     rm -rf $SVN_TMP_DIR
@@ -25,6 +24,9 @@ mkdir $SVN_TMP_DIR && cd $SVN_TMP_DIR
 echo -e "[INFO]\tInitializes Git repository cloning\n"
 git clone $GIT_REPOSITORY_URL $GIT_REPOSITORY_DIR && cd $GIT_REPOSITORY_DIR
 echo -e "\n[INFO]\tTimer start !\n"
+TICKER_MINUTES=0
+TICKER_SECONDS=0
+TICKER_START_TIMESTAMP=$(date +%s)
 while true; do
     sleep 1;
     TICKER_CURRENT_TIMESTAMP=$(date +%s)
@@ -36,17 +38,22 @@ while true; do
         TICKER_MINUTES=$((TICKER_MINUTES + 1))
         TICKER_SECONDS=0
 
-        if ((MINUTES > 0)) && !((MINUTES % TICKER_SVN_UP_MINUTES)); then
-            echo "SVN UP !!!!!"
-            # svn up &
-            # wait %1
-        fi
+        # if ((MINUTES > 0)); then
+
+            if !((MINUTES % TICKER_SVN_UP_MINUTES)); then
+
+                # svn up
+
+                echo "SVN UP !!!!"
+            fi
+            if !((MINUTES % TICKER_SVN_COMMITS_MINUTES)); then
+                for commit in $(git rev-list --reverse $GIT_BRANCH_NAME)
+                do
+                    echo "tour de boucle"
+                    echo $commit
+                done
+            fi
+        # fi
     fi
     echo -e "[DEBUG]\tActive from $TICKER_MINUTES minute(s) and $TICKER_SECONDS second(s)"
 done
-
-# for commit in $(git rev-list --reverse $GIT_BRANCH_NAME)
-# do
-#     echo "tour de boucle"
-#     echo $commit
-# done
